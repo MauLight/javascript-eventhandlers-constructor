9773ceb0ea21742d753ff03735a9c245dcc2778c
MauLight <maulisseluz@gmail.com>
2023-02-04 19:52:04 -0300

    booklist added

--- functions.js
+++ functions.js
@@ -4,6 +4,12 @@ let author;
 let pages;
 let read;
 let newBook;
+const bookArr = [{
+    title: 'The Silmarillion',
+    author: 'JJR. Tolkien',
+    pages: 300,
+    read: false
+}];
 
 function Books(title, author, pages, read) {
     this.title = title;
@@ -23,19 +29,20 @@ function Books(title, author, pages, read) {
     };
 }
 
+
 const titlehandler = (e) => {
-    console.log(e.target.value);
+    //console.log(e.target.value);
     title = e.target.value;
-    console.log(title);
+    //console.log(title);
 };
 
 const authorhandler = (e) => {
-    console.log(e.target.value);
+    //console.log(e.target.value);
     author = e.target.value;
 };
 
 const pageshandler = (e) => {
-    console.log(e.target.value);
+    //console.log(e.target.value);
     pages = e.target.value;
 };
 
@@ -49,15 +56,66 @@ const getPages = document.querySelector('.pages');
 getPages.addEventListener('input', pageshandler);
 
 const handleClick = (myRadio) => {
-    console.log(myRadio.value);
+    //console.log(myRadio.value);
     read = myRadio.value;
 };
 
-const submitData = document.querySelector('.btn');
-submitData.addEventListener('click', () => {
-    newBook = new Books(title, author, pages, read);
-    console.log(newBook.title);
-});
+function submitData(e) {
+    //e.stopImmediatePropagation();
+    if (title != undefined && author != undefined && pages != undefined && read != undefined) {
+        newBook = new Books(title, author, pages, read);
+        console.log(newBook.title);
+        console.log(newBook.author);
+        console.log(newBook.pages);
+        console.log(newBook.read);
+        getTitle.value = '';
+        getAuthor.value = '';
+        getPages.value = '';
+        bookArr.push(newBook);
+        console.log(bookArr);
+    }
+    else {
+        return 0;
+    }
+
+};
+
+function changeDisplay(e) {
+    console.log('Hey!')
+    document.querySelector(".form-group").style.display = "inline";
+
+};
+
+function deleteFunction(e) {
+    console.log("Elv, I love u!");
+}
+
+function sortArr() {
+    let container = document.querySelector('.book-list')
+    let ul = document.createElement('ul');
+    ul.setAttribute('class', 'list-group');
+
+    for (let i = 0; i < bookArr.length; i++) {
+        let li = document.createElement('li');
+        li.innerHTML = bookArr[i].title +
+        " by " +
+        bookArr[i].author +
+        ", " +
+        bookArr[i].pages +
+        " pages, " +
+        (bookArr[i].read ? "already read" : "not read yet") ;
+        li.setAttribute("class", 'list-group-item');
+
+        let button = document.createElement('button');
+        button.setAttribute('class', 'btn btn-outline-dark rounded-0 mx-3');
+        button.setAttribute('onclick', 'deleteFunction()');
+        button.innerHTML = "Delete";
+
+        li.appendChild(button);
+        ul.appendChild(li);
+        container.appendChild(ul);
+    }
+}
 
 
 
